59d2ad74fcb64476a1c7923050d5a7e02c02a43e
Victor <rodionov27@gmail.com>
2020-03-18 07:48:24 +0300

    first commit

---
+++ Untitled-1.js
@@ -0,0 +1,19 @@
+//function showFirstMessage(text) {
+  ////alert(text);
+//}
+//showFirstMessage("Hello World");
+
+
+
+let calc = (a,b)=>a+b
+console.log(calc(3,4));
+
+let str = "test";
+console.log(str.length);
+
+console.log(str.toUpperCase());
+console.log(str.toLowerCase());
+
+let twelve = "12.2px";
+//console.log(Math.round(twelve));
+console.log(parceInt(twelve));
---
+++ script.js
@@ -0,0 +1,17 @@
+'use strict';
+
+
+let incr = 10,
+    decr = 10;
+
+console.log(incr++);
+console.log(decr--);
+
+console.log(5%2);
+console.log("2" === 2);
+
+let isChecked = false;
+let isClose = false;
+
+console.log(isChecked || !isClose);
+
