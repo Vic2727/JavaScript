e7eeeff574c323b0be992d63b71e874c8ff47860
Victor <rodionov27@gmail.com>
2020-03-19 08:23:50 +0300

    script mod

--- script.js
+++ script.js
@@ -1,17 +1,32 @@
 'use strict';
 
+let num = 90;
 
-let incr = 10,
-    decr = 10;
+if(num < 49) {
+  console.log("Неверно")
+} else if(num > 100) {
+  console.log("Многовато")
+} else {
+  console.log("Верно!!!")
+};
 
-console.log(incr++);
-console.log(decr--);
+(num == 50) ? console.log("Верно!!!"):console.log("Неверно");
 
-console.log(5%2);
-console.log("2" === 2);
-
-let isChecked = false;
-let isClose = false;
-
-console.log(isChecked || !isClose);
+switch(num) {
+    case num < 49:
+      console.log("Неверно");
+      break;
+    case num > 100:
+      console.log("Многовато");
+      break;
+    case num > 80:
+      console.log("Все еще много");
+      break;
+    case 50:
+      console.log("Верно");
+      break;
+    default:
+      console.log("Что-то пошло не так");
+      break;
+}
 
